6f87d5cf8f1a27685a4fc71463dffe6d60a8fe5f
Rajesh777-Cyber <bunnypadhu3@gmail.com>
2021-01-12 11:15:28 +0530

    login and register

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'CICDPIPELINE'`, () => {
+  it(`should have as title 'DemoProject'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('CICDPIPELINE');
+    expect(app.title).toEqual('DemoProject');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('CICDPIPELINE app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('DemoProject app is running!');
   });
 });
